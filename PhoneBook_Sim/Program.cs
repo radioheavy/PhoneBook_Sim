d2afc13d4ce80fe7556bed86eaf33631484f01cf
İsmail Oktay Dak <dakoktaytgb@gmail.com>
2022-12-06 13:52:45 +0300

    Add project files.

---
+++ PhoneBook_Sim/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> phoneNumbers = new List<string>();
+
+        while (true)
+        {
+            Console.Write("Enter a phone number (or 'q' to quit): ");
+            string input = Console.ReadLine();
+
+            if (input == "q")
+            {
+                break;
+            }
+
+            phoneNumbers.Add(input);
+        }
+
+        File.WriteAllLines("phone_numbers.txt", phoneNumbers.ToArray());
+
+        string[] lines = File.ReadAllLines("phone_numbers.txt");
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
